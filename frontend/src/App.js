df0e8768cfc66f36c3d9326349000d5c6fc46527
Emmett He <emmetthe26@gmail.com>
2023-04-12 14:09:26 -0700

    frontend for login/register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import LoginForm from './session/loginForm';
+import SignUpForm from './session/signUpForm';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="login" element={<LoginForm />} />
-        {/* <Route path="signin" /> */}
+        <Route path="register" element={<SignUpForm />} />
         <Route path="/" element={'testing react routes'} />
       </Routes>
     </BrowserRouter>
--- frontend/src/session/loginForm.jsx
+++ frontend/src/session/loginForm.jsx
@@ -1,18 +1,43 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 function LoginForm() {
-  const [currentUser, setCurrentUser] = useState({});
+  const [formInput, setFormInput] = useState({
+    username: '',
+    password: ''
+  });
 
-  const changeHandler = () => {};
+  const changeHandler = (e) => {
+    const newInput = {
+      ...formInput,
+      [e.target.name]: e.target.value
+    };
+    setFormInput(newInput);
+  };
+
   const submitHandler = (e) => {
     e.preventDefault();
+    axios
+      .post('http://127.0.0.1:8000/api/login', {
+        username: formInput.username,
+        password: formInput.password
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   };
 
   return (
     <div>
       <form onSubmit={submitHandler}>
-        <input type="text" name="username" onChange={changeHandler} />
-        <input type="password" name="password" onChange={changeHandler} />
+        <p>Username</p>
+        <input type="text" name="username" value={formInput.username} onChange={changeHandler} required />
+
+        <p>Password</p>
+        <input type="password" name="password" value={formInput.password} onChange={changeHandler} required />
         <input type="submit" name="Login" />
       </form>
     </div>
