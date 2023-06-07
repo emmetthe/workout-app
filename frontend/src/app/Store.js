532ae4992e788b14fd0df0c2aa762977582f11e6
Emmett He <emmetthe26@gmail.com>
2023-06-07 14:48:46 -0700

    fix state not persisting on refresh

--- frontend/src/app/Store.js
+++ frontend/src/app/Store.js
@@ -1,14 +1,33 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../slices/authSlice';
-import logger from 'redux-logger'
-import errorReducer from '../slices/errorSlice'
+import logger from 'redux-logger';
+import errorReducer from '../slices/errorSlice';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 
-export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    errors: errorReducer
-  },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+const persistConfig = {
+  key: 'main-root',
+  storage
+};
+const reducers = combineReducers({
+  auth: authReducer,
+  errors: errorReducer
 });
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    }).concat(logger)
+});
+
+const Persistor = persistStore(store);
+
+export { Persistor };
+// export default store;
