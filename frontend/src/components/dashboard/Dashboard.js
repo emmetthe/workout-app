0d59cc1f57c5df2430085df92e87ab9341aaaada
Emmett He <emmetthe26@gmail.com>
2023-06-29 19:22:13 -0700

    added update function to user profile form

--- frontend/src/components/dashboard/Dashboard.js
+++ frontend/src/components/dashboard/Dashboard.js
@@ -8,22 +8,18 @@ import Button from '@mui/material/Button';
 import UpdateProfileForm from './updateProfileForm';
 
 const Dashboard = () => {
-  const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
-    phone: '',
-    city: ''
-  });
-  const { firstName, lastName, phone, city } = formData;
+  // const [formData, setFormData] = useState({
+  //   firstName: '',
+  //   lastName: '',
+  //   phone: '',
+  //   city: ''
+  // });
+  // const { firstName, lastName, phone, city } = formData;
 
   const dispatch = useDispatch();
   const errors = useSelector((state) => state.errors);
   const profile = useSelector((state) => state.auth.profile);
   const modalState = useSelector((state) => state.modal);
-
-  // const onChange = (e) => {
-  //   setFormData({ ...formData, [e.target.name]: e.target.value });
-  // };
 
   // const handleDeleteAccount = (e) => {
   //   e.preventDefault();
@@ -33,10 +29,10 @@ const Dashboard = () => {
   const handleOpen = () => dispatch(showModal(true));
   const handleClose = () => dispatch(showModal(false));
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e, formData) => {
     e.preventDefault();
     handleClose();
-    // dispatch(UpdateProfileAsync({ firstName, lastName, phone, city }));
+    dispatch(UpdateProfileAsync(formData));
     if (errors === '') {
       console.log('Profile Updated Successfully');
     }
@@ -51,7 +47,7 @@ const Dashboard = () => {
       <Button variant="outlined" onClick={handleOpen}>
         Update Profile
       </Button>
-      
+
       <ModalForm
         componentForm={<UpdateProfileForm handleClose={handleClose} handleSubmit={handleSubmit} />}
         modalState={modalState}
