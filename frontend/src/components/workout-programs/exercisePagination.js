1e16e35297884aa3ed34b54ded8e0c6edd2d4bed
Emmett He <emmetthe26@gmail.com>
2023-08-02 15:52:42 -0700

    change pagination button sizing

--- frontend/src/components/workout-programs/exercisePagination.js
+++ frontend/src/components/workout-programs/exercisePagination.js
@@ -8,22 +8,40 @@ const PaginationWrapper = styled('div')({
   marginTop: '16px'
 });
 
+const PaginationButtonStyle = {
+  marginLeft: '2px',
+  marginRight: '2px',
+  minWidth: '80px'
+};
+
 const ExercisePagination = ({ currentPage, totalPages, pageNumbers, handlePageChange }) => {
   return (
     <PaginationWrapper>
-      <Button variant="contained" onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1}>
+      <Button variant="contained" onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1} sx={PaginationButtonStyle}>
         Previous
       </Button>
+
       {pageNumbers.map((pageNumber) => (
         <Button
           key={pageNumber}
           variant={currentPage === pageNumber ? 'contained' : 'outlined'}
           onClick={() => handlePageChange(pageNumber)}
+          sx={PaginationButtonStyle}
         >
           {pageNumber}
         </Button>
       ))}
-      <Button variant="contained" onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === totalPages}>
+
+      <Button
+        variant="contained"
+        onClick={() => handlePageChange(currentPage + 1)}
+        disabled={currentPage === totalPages}
+        sx={{
+          marginLeft: '2px',
+          marginRight: '2px',
+          minWidth: '100px'
+        }}
+      >
         Next
       </Button>
     </PaginationWrapper>
