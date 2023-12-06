b0599aa2ca3a3087f59aab2f5df3d968b6b88bd5
Emmett He <emmetthe26@gmail.com>
2023-12-05 22:25:59 -0800

    change cors origin to false

--- backend/workout_backend/settings.py
+++ backend/workout_backend/settings.py
@@ -62,7 +62,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
 ]
-CORS_ORIGIN_ALLOW_ALL = True   
+CORS_ORIGIN_ALLOW_ALL = False   
 CSRF_TRUSTED_ORIGINS = [
     'https://workout-app-fe.onrender.com',
     # Add any other trusted origins as needed
