a873198fb0b54438f7f2de13e3e13cf18032e60e
Emmett He <emmetthe26@gmail.com>
2024-01-16 01:14:05 -0800

    test csv data in public folder

--- frontend/src/components/exercise/exerciseHome.jsx
+++ frontend/src/components/exercise/exerciseHome.jsx
@@ -27,8 +27,12 @@ const ExerciseHome = () => {
       try {
         // Show the loading indicator while fetching data
         setIsLoading(true);
-        const response = await axiosInstance.get(`/static/workout-data1.csv`);
-        const parsedData = Papa.parse(response.data, { header: true }).data;
+        // const response = await axiosInstance.get(`/static/workout-data1.csv`);
+        // const parsedData = Papa.parse(response.data, { header: true }).data;
+
+        const response = await fetch('data/workout-data1.csv');;
+        const csvData = await response.text();
+        const parsedData = Papa.parse(csvData, { header: true }).data;
 
         // remove double quotes wrapping nested arrays and objects
         const convertedOutput = parsedData.map((data) => convertToObject(data));
