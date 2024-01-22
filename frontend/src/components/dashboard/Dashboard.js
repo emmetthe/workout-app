2c89ddbabcd5a9d7c6816ac6fad5da54da580dc2
Emmett He <emmetthe26@gmail.com>
2024-01-22 15:17:51 -0800

    import useeffect

--- frontend/src/components/dashboard/Dashboard.js
+++ frontend/src/components/dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { UpdateProfileAsync } from '../../slices/authSlice';
